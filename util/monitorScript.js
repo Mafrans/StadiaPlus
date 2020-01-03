50ee97bdb518a4c9eab13a09e69d6f079ca14c3e
DQ <daniel@chaosengine.net>
2020-01-03 07:20:57 -0800

    Update monitorScript.js to v0.5
    from https://pastebin.com/raw/M29RNrLf

--- util/monitorScript.js
+++ util/monitorScript.js
@@ -1,55 +1,27 @@
 // ==UserScript==
 // @name         Stadia Stream Monitor
-// @version      0.3
+// @version      0.5
 // @author       AquaRegia
 // @match        https://stadia.google.com/*
+// @downloadURL  https://pastebin.com/raw/M29RNrLf
 // @grant        none
 // ==/UserScript==
-
 
 (function() {
     'use strict';
 
-    setInterval(() => {
-        let container = document.querySelector(".dSGvzf>div");
-        let btn = document.querySelector("#network_overlay_button");
-        if(!container || btn) {
-            return;
-        }
-
-        let element = document.createElement("div");
-        element.innerHTML = "Toggle Network Overlay";
-        element.id = "network_overlay_button";
-        element.classList.add("stadiaplus_button");
-        element.classList.add("CTvDXd", "QAAyWd", "Fjy05d", "ivWUhc", "QSDHyc", "rpgZzc", "RkyH1e");
-        container.appendChild(element);
-        console.log({container, element});
-
-        element.addEventListener("click", function(e) {
-            let infoBox = document.getElementById("infoBox");
-            infoBox.location = (infoBox.location + 1) % 5;
-    
-            let locations = [
-                "none",
-                "top left",
-                "top right",
-                "bottom right",
-                "bottom left"
-            ];
-    
-            infoBox.classList.add("stadiaplus_overlay");
-            for(let location of locations) {
-                for(let loc of location.split(" ")) {
-                    infoBox.classList.remove(loc);
-                }
-            }
-            for(let loc of locations[infoBox.location].split(" ")) {
-                infoBox.classList.add(loc);
-            }
-        });
-    }, 1000)
-
     function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+
+    function formatTime(seconds)
+    {
+        var hours = Math.floor(seconds / 3600);
+        seconds -= hours*3600;
+
+        var minutes = Math.floor(seconds / 60);
+        seconds -= minutes*60;
+
+        return (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + Math.floor(seconds);
+    }
 
     var peerConnections = [];
 
@@ -65,8 +37,17 @@
     var infoBox = document.createElement("div");
     infoBox.id = "infoBox";
     infoBox.innerHTML = "Start a game to monitor traffic";
+    infoBox.style.position = "fixed";
+    infoBox.style.top = 0;
+    infoBox.style.left = 0;
+    infoBox.style.width = "215px";
+    infoBox.style.opacity = 0.5;
+    infoBox.style.zIndex = 1000;
+    infoBox.style.backgroundColor = "black";
+    infoBox.style.padding = "5px";
 
-    infoBox.location = 0;
+    infoBox.location = 1;
+
     document.body.appendChild(infoBox);
 
     window.addEventListener("keydown", function(e)
@@ -75,28 +56,47 @@
         {
             infoBox.location = (infoBox.location + 1) % 5;
 
-            let locations = [
-                "none",
-                "top left",
-                "top right",
-                "bottom right",
-                "bottom left"
-            ];
-
-            infoBox.classList.add("stadiaplus_overlay");
-            for(let location of locations) {
-                for(let loc of location.split(" ")) {
-                    infoBox.classList.remove(loc);
-                }
-            }
-            for(let loc of locations[infoBox.location].split(" ")) {
-                infoBox.classList.add(loc);
+            switch(infoBox.location)
+            {
+                case 0:
+                    infoBox.style.display = "none";
+                    break;
+                case 1:
+                    infoBox.style.top = 0;
+                    infoBox.style.right = "";
+                    infoBox.style.bottom = "";
+                    infoBox.style.left = 0;
+                    infoBox.style.display = "block";
+                    break;
+                case 2:
+                    infoBox.style.top = 0;
+                    infoBox.style.right = 0;
+                    infoBox.style.bottom = "";
+                    infoBox.style.left = "";
+                    infoBox.style.display = "block";
+                    break;
+                case 3:
+                    infoBox.style.top = "";
+                    infoBox.style.right = 0;
+                    infoBox.style.bottom = 0;
+                    infoBox.style.left = "";
+                    infoBox.style.display = "block";
+                    break;
+                case 4:
+                    infoBox.style.top = "";
+                    infoBox.style.right = "";
+                    infoBox.style.bottom = 0;
+                    infoBox.style.left = 0;
+                    infoBox.style.display = "block";
+                    break;
             }
         }
     });
 
     var lastBytes = 0;
     var lastFrames = 0;
+    var sessionStart;
+    var active = false;
 
     setInterval(function()
     {
@@ -105,11 +105,18 @@
             peerConnections = [];
             lastBytes = 0;
             lastFrames = 0;
+            active = false;
 
             infoBox.innerHTML = "Start a game to monitor traffic";
         }
         else if(peerConnections.length == 3)
         {
+            if(!active)
+            {
+                sessionStart = new Date();
+                active = true;
+            }
+
             peerConnections[2].getStats().then(function(stats)
             {
                 for(var key of stats.keys())
@@ -126,7 +133,10 @@
                                 return "ssrc" == f.type && f.id.endsWith("recv") && f.names().includes("mediaType") && "video" == f.stat("mediaType");
                             });
 
+                            console.log(sessionStart);
+
                             var time = new Date();
+                            var sessionDuration = (time - sessionStart) / 1000;
                             time = new Date(time - time.getTimezoneOffset() * 60 * 1000).toISOString().replace("T", " ").split(".")[0];
                             var resolution = tmp2.frameWidth + "x" + tmp2.frameHeight;
                             var framesReceived = tmp2.framesReceived;
@@ -134,29 +144,53 @@
                             var codec = tmp3.stat("googCodecName");
                             var bytesReceived = tmp1.bytesReceived;
                             var bytesReceivedPerSecond = (bytesReceived - lastBytes);
+                            var averageData = ((((bytesReceived / sessionDuration) * 3600) / 1024) / 1024) / 1024;
                             var packetsLost = tmp1.packetsLost;
                             var framesDropped = tmp2.framesDropped;
                             var latency = tmp3.stat("googCurrentDelayMs");
+                            var jitterBuffer = tmp3.stat("googJitterBufferMs");
 
                             lastFrames = framesReceived;
                             lastBytes = bytesReceived;
 
                             if(framesReceived > 0)
                             {
-                                var html = `
-                                    <h5>${time}</h5>
-                                    <hr>
-                                    <p>
-                                        Resolution: ${resolution}<br/>
-                                        FPS: ${framesReceivedPerSecond}<br/>
-                                        Codec ${codec}<br/>
-                                        Session traffic: ${formatBytes(bytesReceived)}<br/>
-                                        Current traffic: ${formatBytes(bytesReceivedPerSecond*8, 2).slice(0, -1)} b/s<br/>
-                                        Packets lost: ${packetsLost}<br/>
-                                        Frames dropped: ${framesDropped}<br/>
-                                        Latency: ${latency} ms
-                                    </p>
-                                `;  
+                                var html = "";
+
+                                html += "<b>" + time + "</b>";
+                                html += "<br/>";
+
+                                html += "Session time: " + formatTime(sessionDuration);
+                                html += "<br/>";
+
+                                html += "Resolution: " + resolution;
+                                html += "<br/>";
+
+                                html += "FPS: " + framesReceivedPerSecond;
+                                html += "<br/>";
+
+                                html += "Codec: " + codec;
+                                html += "<br/>";
+
+                                html += "Session traffic: " + formatBytes(bytesReceived, 2);
+                                html += "<br/>";
+
+                                html += "Current traffic: " + formatBytes(bytesReceivedPerSecond*8, 2).slice(0, -1) + "b/s";
+                                html += "<br/>";
+
+                                html += "Average traffic: " + averageData.toFixed(2) + " GB/h";
+                                html += "<br/>";
+
+                                html += "Packets lost: " + packetsLost;
+                                html += "<br/>";
+
+                                html += "Frames dropped: " + framesDropped;
+                                html += "<br/>";
+
+                                html += "Latency: " + latency + "ms";
+                                html += "<br/>";
+
+                                html += "Jitter buffer: " + jitterBuffer + "ms";
 
                                 infoBox.innerHTML = html;
                             }
@@ -165,5 +199,5 @@
                 }
             });
         }
-    }, 500);
+    }, 1000);
 })();
