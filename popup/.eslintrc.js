6346a3a22f8f9016c70d68646809dfcf40a33156
Mafrans <malte.kluft@gmail.com>
2020-01-13 09:21:24 +0100

    Change Vue template to support extension popups

--- popup/.eslintrc.js
+++ popup/.eslintrc.js
@@ -1,28 +1,34 @@
 module.exports = {
-    extends: "airbnb",
-    env: {
-        "browser": true
-    },
-    globals: {
-        "SlimSelect": true
-    },
-    rules: {
-      "func-names": [
-          "error",
-          "never"
-      ],
-      "indent": [
-          "error",
-          4
-      ],
-      "no-console": [
-          "off"
-      ],
-      "no-use-before-define": [
-          "warn"
-      ],
-      "linebreak-style": [
-          "off"
-      ]
-    }
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    webextensions: true,
+  },
+  extends: ['plugin:vue/essential'],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "func-names": [
+        "error",
+        "never"
+    ],
+    "indent": [
+        "error",
+        4
+    ],
+    "no-console": [
+        "off"
+    ],
+    "no-use-before-define": [
+        "warn"
+    ],
+    "linebreak-style": [
+        "off"
+    ]
+  }
 }
---
+++ popup/src/popup.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+import router from '../router'
+
+Vue.prototype.$browser = global.browser
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    
+    router,
+    render: h => h(App)
+})
