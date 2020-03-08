df2fa7ad72933d80df6a247dc7453d2e05be194b
Mafrans <malte.kluft@gmail.com>
2020-03-08 21:11:14 +0100

    Funny messages

--- injectors/tabInjector.js
+++ injectors/tabInjector.js
@@ -1,5 +1,24 @@
 (() => {
     const { UIComponent, UIButton, chrome } = window;
+    const motdList = [
+        '<img src="https://cdn.discordapp.com/emojis/636227864076746772.png?v=1" style="display: inline-flex; width: 24px">',
+        'Don\'t look here, look below!',
+        'Reality can be whatever I want.',
+        'If you ask nicely, Stadia might even do your laundry for you!',
+        'Have you tried asking customer support?',
+        'Follow u/mafrans on Reddit!',
+        'Stadia+, like Google+ but still alive.',
+        'Stadia is DOA, believe me.',
+        'Get a load of this guy, using chrome extensions to improve his Stadia experience.',
+        'I\'m telling you, don\'t trust that guy. You know, the guy.',
+        'Shoutout to Chris and Grace!',
+        'Have you tried Splitlings yet?',
+        'Always check the ratings before you buy a new game.',
+        'Have you tried GeForce Now?',
+        'A hot dog is definitely not a sandwich.',
+        'Idk about you, but I really want one of those fancy smart toilets.',
+    ];
+
     window.addEventListener('load', () => {
         const component = new UIComponent(
             'Stadia+',
@@ -8,7 +27,7 @@
                     class="stadiaplus_muted" 
                     style="margin-top: 1rem; margin-bottom: 5rem; display: block"
                 >
-                    Don't look here, look below!
+                    ${motdList[Math.floor(Math.random() * motdList.length)]}
                 </i>
                 
                 <p>Network Overlay</p>
