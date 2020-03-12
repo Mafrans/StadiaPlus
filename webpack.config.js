a25add4126056e312a818c8f52436cbc9d98dad3
Mafrans <malte.kluft@gmail.com>
2020-03-12 15:24:18 +0100

    Enable hacker mode 😎

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
+    mode: 'development',
     module: {
         rules: [
             {
