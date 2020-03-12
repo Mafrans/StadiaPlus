74fd21924038c16f05b5473bc0ad8f11f925ed4b
Mafrans <malte.kluft@gmail.com>
2020-03-12 15:54:18 +0100

    Allow drawing images in the console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import Logger from './logger';
+import Logger from './Logger';
+
+Logger.image('https://ssl.gstatic.com/ygp/ui/img/stadia_logo_512.png', 100, 100)
 
 Logger.info('Test info message');
 Logger.warning('Test warning message');
--- src/logger.ts
+++ src/logger.ts
@@ -12,6 +12,22 @@ class Logger {
     error = function(...obj: any[]) {
         console.log(`${prefix} %c❌ ${obj.join(' ')}`, 'color: red; font-weight: 700');
     };
+
+    /**
+     * Dubiously created by Adrian Cooney
+     * http://adriancooney.github.io
+     */
+    image = function(url: string, width: number, height: number) {
+        const getBox = function(width:number, height:number) {
+            return {
+                string: "+",
+                style: "font-size: 1px; padding: " + Math.floor(height/2) + "px " + Math.floor(width/2) + "px; line-height: 0;"
+            }
+        }
+
+        var dim = getBox(width, height);
+        console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (width) + "px " + (height) + "px; color: transparent;");
+    };
 }
 
 export default new Logger();
