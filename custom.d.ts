c459ea2b86922a037efc09231c23f1cc638518b1
Mafrans <malte.kluft@gmail.com>
2020-03-12 14:05:45 +0100

    Generic webpack boilerplate

---
+++ custom.d.ts
@@ -0,0 +1,24 @@
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.css" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.scss" {
+    const content: any;
+    export default content;
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
