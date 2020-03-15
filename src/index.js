b6ff492329487b0d5160f8f9a34689573c6b11f7
Mafrans <malte.kluft@gmail.com>
2020-03-15 22:36:02 +0100

    2.0 Beta 2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Logger from './Logger';
 import Util from './Util';
 import { ComponentLoader } from './ComponentLoader';
 import { Clock } from './components/Clock'
+import { UITab } from './components/UITab'
 
 const loader = new ComponentLoader();
 
 loader.register(new Clock());
+loader.register(new UITab());
 
 window.addEventListener('load', () => {
     Util.load();
