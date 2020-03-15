c48fc8d04a12e1c4f39ce0809caa14dbe4aa9e74
Mafrans <malte.kluft@gmail.com>
2020-03-15 13:40:41 +0100

    I gave up, running a query every second is easier

--- src/Component.ts
+++ src/Component.ts
@@ -4,15 +4,16 @@ export class Component {
     name: string = 'MyComponent';
     id: string;
     enabled: boolean;
-    observeTarget: Element;
-    exists: boolean;
 
     load(): void {
         this.id =
             this.name.toLowerCase() + '-' + Math.floor(Math.random() * 999999);
 
-        this.exists = document.getElementById(this.id) !== null;
         this.onStart();
+    }
+
+    exists(): boolean {
+        return document.querySelector('#' + this.id) !== null;
     }
 
     unload(): void {
