9ea17d42bbed6004017d4886a8dbb7fe3a7f2d63
Mafrans <malte.kluft@gmail.com>
2020-03-15 12:56:46 +0100

    Make sure everything runs again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Logger from './Logger';
+import { ComponentLoader } from './ComponentLoader';
+import { Clock } from './components/Clock'
 
-Logger.image('https://ssl.gstatic.com/ygp/ui/img/stadia_logo_512.png', 100, 100)
+const loader = new ComponentLoader();
 
-Logger.info('Test info message');
-Logger.warning('Test warning message');
-Logger.error('Test error message');
+loader.register(new Clock());
+loader.start();
