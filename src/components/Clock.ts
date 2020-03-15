b7be8ea214fcb5bf4dc032d36e34af4271d7dc85
Mafrans <malte.kluft@gmail.com>
2020-03-15 13:57:30 +0100

    Add simple unstyled clock

--- src/components/Clock.ts
+++ src/components/Clock.ts
@@ -3,17 +3,46 @@ import Logger from '../Logger';
 
 export class Clock extends Component {
     name: string;
-    id: string;
-    enabled: boolean;
+    element: HTMLElement;
+    container: HTMLElement;
+    menuElement: HTMLElement;
 
     constructor() {
         super();
+
+        this.name = "Clock";
+        this.createElement();
+    }
+
+    createElement() {
+        this.element = document.createElement('span');
+        this.element.classList.add('stadiaplus_clock');
     }
 
     onStart(): void {
         this.enabled = true;
-        Logger.component('Component', name, 'has been enabled');
+        this.element.id = this.id;
+        this.container = document.querySelector('.hxhAyf');
+        this.menuElement = document.querySelector('.X1asv');
+
+        Logger.component('Component', this.name, 'has been enabled');
     }
-    onStop(): void {}
-    onMutation(event: MutationEvent): void {}
+
+    onStop(): void {
+        Logger.component('Component', this.name, 'has been disabled');
+    }
+
+    onUpdate() {
+        // Only update the clock when it's visible
+        if(this.menuElement.style['opacity'] !== '0') {
+            if(!this.exists() && this.container !== null) {
+                this.container.prepend(this.element);
+            }
+
+            const time = new Date().toLocaleTimeString();
+            window.requestAnimationFrame(() => {
+                this.element.innerHTML = time;
+            });
+        }
+    }
 }
