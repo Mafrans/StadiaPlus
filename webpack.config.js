90cf9c88a4468bbb9481e228784155c872316c0c
Mafrans <malte.kluft@gmail.com>
2020-03-15 15:50:49 +0100

    Allow style loading

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,12 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    'style-loader',
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insert: 'html',
+                        }
+                    },
                     // Translates CSS into CommonJS
                     'css-loader',
                     // Compiles Sass to CSS
