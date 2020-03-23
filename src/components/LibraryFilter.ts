645691a20b23b7dee1ae5f66ecf77295b3952040
Mafrans <malte.kluft@gmail.com>
2020-03-23 19:40:34 +0100

    Remove unnecessary method

--- src/components/LibraryFilter.ts
+++ src/components/LibraryFilter.ts
@@ -15,11 +15,10 @@ const iconInvisible = chrome.runtime.getURL('images/icons/visibility_off.svg');
  * @extends {Component}
  */
 export class LibraryFilter extends Component {
-
     /**
      * The name of the Component.
      */
-    name: string = "Library Filter";
+    name: string = 'Library Filter';
 
     /**
      * The clock element.
@@ -37,17 +36,25 @@ export class LibraryFilter extends Component {
      * Creates a simple <span>, adds the right classes, and stores it in [@link #element]
      */
     createAllWrappers() {
-        setTimeout(()=>{
-            const gameTiles = document.querySelectorAll(".GqLi4d");
-            gameTiles.forEach(tile => {
-                this.createWrapper(tile);
-            })
-        }, 20000)
+        const self = this;
+        setTimeout(() => {
+            const gameTiles = document.querySelectorAll('.GqLi4d');
+            this.getStorage(() => {
+                gameTiles.forEach((tile) => {
+                    const uuid = tile
+                        .getAttribute('jslog')
+                        .split('; ')[1]
+                        .substring(3);
+                    self.createWrapper(tile, uuid);
+                });
+            });
+        }, 7500);
     }
 
-    createWrapper(element: Element) {
+    createWrapper(element: Element, uuid: string) {
         const wrapper = document.createElement('div');
         wrapper.classList.add('stadiaplus_libraryfilter-wrapper');
+        wrapper.id = this.id + '-' + uuid;
 
         const icon = document.createElement('div');
         icon.classList.add('stadiaplus_libraryfilter-icon');
@@ -57,18 +64,37 @@ export class LibraryFilter extends Component {
         wrapper.appendChild(icon);
         wrapper.appendChild(element);
 
-        const uuid = element.getAttribute('jslog').split('; ')[1].substring(3);
-        Logger.info(uuid);
-        if(this.games[uuid] === undefined) {
+        if (!this.games.hasOwnProperty(uuid)) {
             this.games[uuid] = { visible: true };
+        } else if (!this.games[uuid].visible) {
+            wrapper.classList.add('closed');
         }
-        
+
         icon.style.marginLeft = element.clientWidth - icon.clientWidth + 'px';
         icon.addEventListener('click', () => {
             wrapper.classList.add('closing');
 
-            setTimeout(() => wrapper.classList.add('closed'), 1000);
-        })
+            setTimeout(() => {
+                wrapper.classList.add('closed');
+                wrapper.classList.remove('closing');
+
+                this.games[uuid].visible = false;
+                this.setStorage();
+            }, 1000);
+        });
+    }
+
+    getStorage(callback?: Function) {
+        chrome.storage.sync.get(['games'], (result: any) => {
+            this.games = result.games !== undefined ? result.games : {};
+
+            if (callback) callback();
+        });
+    }
+
+    setStorage(callback?: Function) {
+        const self = this;
+        chrome.storage.sync.set({ games: self.games }, callback);
     }
 
     /**
@@ -86,13 +112,14 @@ export class LibraryFilter extends Component {
      */
     onStop(): void {
         this.enabled = false;
-        document.querySelectorAll('.stadiaplus_libraryfilter-eye').forEach(e => e.remove());
+        document
+            .querySelectorAll('.stadiaplus_libraryfilter-eye')
+            .forEach((e) => e.remove());
         Logger.component('Component', this.name, 'has been disabled');
     }
 
     /**
      * Called every second, updates the element to match the clock.
      */
-    onUpdate() {
-    }
+    onUpdate() {}
 }
