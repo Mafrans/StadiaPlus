6a42d288dbe2ace39ea521250d00e469e79ae6be
Mafrans <malte.kluft@gmail.com>
2020-03-25 20:51:10 +0100

    Create simple cached json database

---
+++ src/Database.ts
@@ -0,0 +1,58 @@
+import Logger from "./Logger";
+
+export class Database {
+    url: string;
+    connected: boolean;
+    connection: object;
+    
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    connect(callback?: (connection:object) => {}) {
+        if(this.connected) {
+            Logger.error('Error: Already connected to the database.');
+            return;
+        }
+
+        const self = this;
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", this.url, true);
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    self.connected = true;
+                    self.connection = JSON.parse(xhr.responseText);
+                    callback(self.connection);
+                }
+                else {
+                    self.connected = false;
+                    Logger.error('Error when connecting to database:', xhr.statusText);
+                }
+            }
+        };
+        xhr.onerror = function (e) {
+            self.connected = false;
+            Logger.error('Error when connecting to database:', xhr.statusText);
+        };
+        xhr.send(null); 
+    }
+
+    getConnection(): object {
+        if(!this.connected) {
+            Logger.error('Error: Not connected to the database');
+            return null;
+        }
+        return this.connection;
+    }
+
+    disconnect() {
+        this.connection = null;
+        this.connected = false;
+    }
+
+    reconnect(callback?: (connection:object) => {}) {
+        this.disconnect();
+        this.connect(callback);
+    }
+}
