2951e85fb602e5ba113b3f127ef405c23b6b1c73
Mafrans <malte.kluft@gmail.com>
2020-04-18 03:26:37 +0200

    Make a proper Settings page

--- src/popup/src/SettingsPage.vue
+++ src/popup/src/SettingsPage.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="container">
-        <page-header :back-button="true">Settings</page-header>
+    <div class="settings-page">
+        <div class="container">
+            <page-header :back-button="true">Settings</page-header>
 
-        <footer>
-            <span>Stadia+ 2.1</span>
-            <span>Developed by <a href="https://reddit.com/u/Mafrans">Mafrans</a></span>
-        </footer>
+            <div class="row">
+                <div class="col">
+                    <icon :style="{ float: 'left', marginRight: '4px' }"
+                        >language</icon
+                    >
+                    <h3 :style="{ marginBottom: '0.5rem' }">Language</h3>
+                    <select-box
+                        :items="languages"
+                        :selectstyle="SelectStyle.SLIMSELECT_LARGE"
+                    />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,43 +23,42 @@
 import Icon from './components/Icon.vue';
 import PageHeader from './components/PageHeader.vue';
 import Button from './components/Button.vue';
+import SelectBox from './components/SelectBox.vue';
+import { Language } from '../../Language';
 import logo from './assets/logo.png';
+import { SelectStyle } from '../../ui/Select';
 export default {
     data() {
         return {
-            logo: logo
-        }
+            logo: logo,
+            languages: Language.languages.map((lang) => {
+                return { value: lang.tag, label: lang.name };
+            }),
+            SelectStyle: SelectStyle,
+        };
+    },
+    created() {
+        console.log(this.languages);
     },
     components: {
         Icon,
         PageHeader,
-        btn: Button
+        btn: Button,
+        SelectBox,
     },
     methods: {
         settings() {
             this.$router.push('settings');
         },
         open(url) {
-            window.open(url,'_blank');
-        }
-    }
+            window.open(url, '_blank');
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
     padding: 1rem;
-}
-
-footer {
-    display: flex;
-    justify-content: space-between;
-    opacity: 0.5;
-    margin-top: 1rem;
-
-    span, a {
-        color: #000000;
-        font-size: 12px;
-    }
 }
 </style>
