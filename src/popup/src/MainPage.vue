7c571001929deb6238dc45850190ae783ffdb19f
Mafrans <malte.kluft@gmail.com>
2020-04-18 03:27:09 +0200

    Add buttons and remove footer

--- src/popup/src/MainPage.vue
+++ src/popup/src/MainPage.vue
@@ -1,47 +1,59 @@
 <template>
-    <div class="container">
-        <page-header :img="logo" icon="settings" v-on:icon-click="settings">Stadia+</page-header>
-        <p>
-            The extension is all ready to go. Just fire up Stadia and start
-            playing! 🎮
-        </p>
+    <div class="main-page">
+        <div class="container">
+            <page-header :img="logo" icon="settings" v-on:icon-click="settings"
+                >Stadia+</page-header
+            >
+            <p>
+                The extension is all ready to go. Just fire up Stadia and start
+                playing! 🎮
+            </p>
 
-        <div class="row">
-            <div class="col">
-                <btn v-on:click="open('https://stadia.google.com')" :gradient="true">
-                    <icon>launch</icon>
-                    Launch Stadia
-                </btn>
+            <div class="row">
+                <div class="col">
+                    <btn
+                        v-on:click="open('https://stadia.google.com')"
+                        :gradient="true"
+                    >
+                        <icon>launch</icon>
+                        Launch Stadia
+                    </btn>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <btn
+                        v-on:click="
+                            open('https://github.com/Mafrans/StadiaPlus/wiki')
+                        "
+                    >
+                        <icon>help_outline</icon>
+                        Help & FAQ
+                    </btn>
+                </div>
+            </div>
+
+            <hr />
+
+            <div class="row">
+                <div class="col">
+                    <btn v-on:click="open('https://discord.gg/2VDbEQ8')">
+                        Discord
+                    </btn>
+                </div>
+                <div class="col">
+                    <btn
+                        v-on:click="
+                            open(
+                                'https://www.reddit.com/r/Stadia/comments/fz7xgm/stadia_20_what_youve_all_been_waiting_for/'
+                            )
+                        "
+                    >
+                        Reddit
+                    </btn>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <btn v-on:click="open('https://github.com/Mafrans/StadiaPlus/wiki')">
-                    <icon>help_outline</icon>
-                    Help & FAQ
-                </btn>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col">
-                <btn v-on:click="open('https://discord.gg/2VDbEQ8')">
-                    Discord
-                </btn>
-            </div>
-            <div class="col">
-                <btn v-on:click="open('https://www.reddit.com/r/Stadia/comments/fz7xgm/stadia_20_what_youve_all_been_waiting_for/')">
-                    Reddit
-                </btn>
-            </div>
-        </div>
-
-        <footer>
-            <span>Stadia+ 2.1</span>
-            <span>Developed by <a href="https://reddit.com/u/Mafrans">Mafrans</a></span>
-        </footer>
     </div>
 </template>
 
@@ -53,39 +65,27 @@ import logo from './assets/logo.png';
 export default {
     data() {
         return {
-            logo: logo
-        }
+            logo: logo,
+        };
     },
     components: {
         Icon,
         PageHeader,
-        btn: Button
+        btn: Button,
     },
     methods: {
         settings() {
             this.$router.push('settings');
         },
         open(url) {
-            window.open(url,'_blank');
-        }
-    }
+            window.open(url, '_blank');
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
     padding: 1rem;
-}
-
-footer {
-    display: flex;
-    justify-content: space-between;
-    opacity: 0.5;
-    margin-top: 1rem;
-
-    span, a {
-        color: #000000;
-        font-size: 12px;
-    }
 }
 </style>
