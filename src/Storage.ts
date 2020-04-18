fd1d7c8671a51f7ee49a2881f0ce64c254a036d9
Mafrans <malte.kluft@gmail.com>
2020-04-18 13:02:05 +0200

    Create simple Storage abstraction layer

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,3 +1,50 @@
+const { chrome } = window as any;
+
 export class Storage {
+    static Local: LocalStorage;
+    static Sync: SyncStorage;
+}
+
+class LocalStorage {
+    name: string;
+    tag: string;
     
+    constructor(name: string, tag: string) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    get(callback: (result: any) => any) {
+        chrome.storage.local.get([ this.tag ], callback);
+    }
+
+    set(value: any, callback: () => any = () => {}) {
+        chrome.storage.local.set({ [this.tag]: value }, callback);
+    }
+
+    static get(storages: LocalStorage[], callback: (result: any) => any) {
+        chrome.storage.local.get(storages.map(e => e.tag), callback);
+    }
+}
+
+class SyncStorage {
+    name: string;
+    tag: string;
+    
+    constructor(name: string, tag: string) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    get(callback: (result: any) => any) {
+        chrome.storage.sync.get([ this.tag ], callback);
+    }
+
+    set(value: any, callback: () => any = () => {}) {
+        chrome.storage.sync.set({ [this.tag]: value }, callback);
+    }
+
+    static get(storages: LocalStorage[], callback: (result: any) => any) {
+        chrome.storage.sync.get(storages.map(e => e.tag), callback);
+    }
 }
