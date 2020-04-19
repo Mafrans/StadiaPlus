400dfdc09cd2894d8f1e350b2559520219cb9673
Mafrans <malte.kluft@gmail.com>
2020-04-19 20:36:29 +0200

    Allow manual cache clearing

---
+++ src/popup/src/DeveloperPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="developer-page">
+        <div class="container">
+            <page-header :back-button="true">{{
+                Language.get('popup.developer-page.title')
+            }}</page-header>
+
+            <div class="row">
+                <div class="col">
+                    <h3>{{ Language.get('popup.developer-page.storage') }}</h3>
+                    <btn
+                        v-on:click="
+                            clearStorage()
+                        "
+                    >
+                        <icon>clear_all</icon>
+                        {{ Language.get('popup.developer-page.clear-cache-button') }}
+                    </btn>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Icon from './components/Icon.vue';
+import PageHeader from './components/PageHeader.vue';
+import Button from './components/Button.vue';
+import { Language } from '../../Language';
+import { SyncStorage, LocalStorage } from '../../Storage';
+export default {
+    data() {
+        return {
+            Language: Language,
+        };
+    },
+    components: {
+        Icon,
+        PageHeader,
+        btn: Button,
+    },
+    methods: {
+        clearStorage() {
+            LocalStorage.clear();
+            SyncStorage.clear();
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+    padding: 1rem;
+}
+</style>
