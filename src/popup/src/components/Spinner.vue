96ddf3e8d1dbb0c7293cc05cca9aa8dce7845b7f
Mafrans <malte.kluft@gmail.com>
2020-04-20 21:53:29 +0200

    Add a spinner for the future

---
+++ src/popup/src/components/Spinner.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="loader">
+        <div v-for="i in 4" :key="i" :style="{'animation-delay': i * 0.125 + 's'}"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'color'
+    ]
+};
+</script>
+
+<style lang="scss" scoped>
+.loader div {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    border: 3px black solid;
+    clip-path: inset(50% 50% 0% 0%);
+    transform: rotate(0deg);
+    box-sizing: border-box;
+    animation: loading 1.5s infinite;
+}
+
+@keyframes loading {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
