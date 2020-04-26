4843ed49b70422f7c9eaf173ac133b9462c29bdf
Mafrans <malte.kluft@gmail.com>
2020-04-26 21:40:08 +0200

    Add top margin

--- src/popup/src/SettingsPage.vue
+++ src/popup/src/SettingsPage.vue
@@ -24,7 +24,7 @@
 
             <div class="row">
                 <div class="col">
-                    <h3 :style="{ marginBottom: '0.5rem' }">
+                    <h3 :style="{ marginBottom: '0.5rem', marginTop: '1rem' }">
                         {{ Language.get('popup.settings-page.components') }}
                     </h3>
                     <page-button next="settings/components">{{ Language.get('popup.settings-page.edit-components') }}</page-button>
