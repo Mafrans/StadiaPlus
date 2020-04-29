60022ee3d90923428e1ff41c984fef37026538ff
Mafrans <malte.kluft@gmail.com>
2020-04-29 16:34:33 +0200

    make event tracking less of a bother

--- src/util/EventTracker.ts
+++ src/util/EventTracker.ts
@@ -2,7 +2,7 @@ export class EventTracker {
     target: EventTarget;
 
     private original: any;
-    constructor(target: EventTarget, _listener: ((type: string, listener: any, options?: any) => any) = (() => {})) {
+    constructor(target: EventTarget, _listener: ((type: string, listener?: any, options?: any) => any) = (() => {})) {
         this.original = target.addEventListener;
         this.target = target;
 
