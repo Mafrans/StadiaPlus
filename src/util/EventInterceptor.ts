84fdef747d35e08659ed1bafce1d5df6cd657262
Mafrans <malte.kluft@gmail.com>
2020-04-29 16:31:59 +0200

    Add Event Tracker for fullscreen intercepting

---
+++ src/util/EventInterceptor.ts
@@ -0,0 +1,18 @@
+export class EventTracker {
+    target: EventTarget;
+
+    private original: any;
+    constructor(target: EventTarget, _listener: ((type: string, listener: any, options?: any) => any) = (() => {})) {
+        this.original = target.addEventListener;
+        this.target = target;
+
+        target.addEventListener = function(type, listener, options?) {
+            _listener(type, listener, options);
+            this.original(type, listener, options);
+        };
+    }
+
+    remove() {
+        this.target.addEventListener = this.original;
+    }
+}
