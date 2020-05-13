b5176d6ef36c04ccf69c7c0949144a0655a54c60
Mafrans <malte.kluft@gmail.com>
2020-05-13 19:08:05 +0200

    How the hell is hashing done

--- src/Browser.ts
+++ src/Browser.ts
@@ -1,8 +1,16 @@
 export class Browser {
     private static version: number;
     static init() {
-        const version = (navigator.appVersion.split(' ') as any).find((e: string) => e.indexOf('Chrome') != -1).split('/')[1];
-        this.version = parseInt(version.replace(/\./g, ''));
+        const version = (navigator.appVersion.split(' ') as any).find((e: string) => e.indexOf('Chrome') != -1).split('/')[1].split('.');
+
+        let accumulator = 0;
+        for(let i = 0; i < version.length; i++) {
+            console.log(version[i] * Math.pow(10, (version.length - i - 1) * 2));
+            accumulator += version[i] * Math.pow(10, (version.length - i - 1) * 2);
+        }
+
+        console.log(accumulator);
+        this.version = accumulator;
     }
 
     static getVersion() {
