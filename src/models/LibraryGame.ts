597762f49b8703b26f529fc42143346237aa1410
Eric <eric.lowry@digital-skin.net>
2020-11-20 00:29:04 +0100

    Library item background animation now uses transform for better scalability.

--- src/models/LibraryGame.ts
+++ src/models/LibraryGame.ts
@@ -42,6 +42,13 @@ export class LibraryGame {
             .class({ 'stadiaplus_libraryfilter-game': true })
             .attr({ 'data-uuid': this.uuid, 'tab-index': 0 })
             .child(
+                $el('div')
+                    .class({ 'tile-background': true })
+                    .css({
+                        'background-image': this.img !== '' ? `url(${this.img})` : '',
+                    }),
+            )
+            .child(
                 $el('img')
                     .class({ 'play-button': true })
                     .attr({
@@ -60,9 +67,9 @@ export class LibraryGame {
                     .class({ content: true })
                     .child($el('h6').text(this.name)),
             )
+            .class({ tileBackground: true })
             .css({
-                display: this.visible ? '' : 'none',
-                'background-image': this.img !== '' ? `url(${this.img})` : '',
+                display: this.visible ? '' : 'none'
             });
 
         const moreDropdown = this.getMoreDropdown();
