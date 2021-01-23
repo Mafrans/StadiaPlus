fa382ccd1060bb35690a9390f1bf5ac6154677db
Mafrans <malte.kluft@gmail.com>
2021-01-23 14:54:18 +0100

    fix: update configurations to better fit new project structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,105 +1,22 @@
-const { resolve } = require('path');
-module.exports = {
-    // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-    // This option interrupts the configuration hierarchy at this file
-    // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-    root: true,
-
-    // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
-    // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-    // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
-    parserOptions: {
-        // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-        // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
-        // Needed to make the parser take into account 'vue' files
-        extraFileExtensions: ['.vue'],
-        parser: '@typescript-eslint/parser',
-        project: resolve(__dirname, './tsconfig.json'),
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module' // Allows for the use of imports
+export default {
+    "rules": {},
+    "env": {
+        "es6": true,
+        "browser": true
     },
-
-    env: {
-        browser: true
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
     },
-
-    // Rules order is important, please avoid shuffling them
-    extends: [
-        // Base ESLint recommended rules
-        // 'eslint:recommended',
-
-        // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-        // ESLint typescript rules
-        'plugin:@typescript-eslint/recommended',
-        // consider disabling this class of rules if linting takes too long
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-
-        // Uncomment any of the lines below to choose desired strictness,
-        // but leave only one uncommented!
-        // See https://eslint.vuejs.org/rules/#available-rules
-        'plugin:vue/essential', // Priority A: Essential (Error Prevention)
-        'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-        // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-        'airbnb-base'
-
-    ],
-
-    plugins: [
-        // required to apply rules which need type information
-        '@typescript-eslint',
-
-        // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-        // required to lint *.vue files
-        'vue',
-
-    ],
-
-    globals: {
-        ga: true, // Google Analytics
-        cordova: true,
-        __statics: true,
-        process: true,
-        Capacitor: true,
-        chrome: true
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-
-    // add your custom rules here
-    rules: {
-        'no-param-reassign': 'off',
-        'indent': ['warn', 4, { "SwitchCase": 1 }],
-        'no-void': 'off',
-        'no-multiple-empty-lines': 'warn',
-        'lines-between-class-members': 'off',
-        "no-shadow": "off",
-
-        'import/first': 'off',
-        'import/named': 'error',
-        'import/namespace': 'error',
-        'import/default': 'error',
-        'import/export': 'error',
-        'import/extensions': 'off',
-        'import/no-unresolved': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'import/prefer-default-export': 'off',
-        'import/no-webpack-loader-syntax': 'off',
-        'prefer-promise-reject-errors': 'off',
-        'max-classes-per-file': 'off',
-        'class-methods-use-this': 'off',
-        'no-restricted-globals': 'warn',
-
-        // TypeScript
-        quotes: ['warn', 'single', { avoidEscape: true }],
-        "@typescript-eslint/no-shadow": ["error"],
-        '@typescript-eslint/ban-ts-comment': 'off',
-        // '@typescript-eslint/explicit-function-return-type': 'off',
-        // '@typescript-eslint/explicit-module-boundary-types': 'off',
-        // '@typescript-eslint/no-unsafe-call': 'warn',
-        // '@typescript-eslint/no-unsafe-member-access': 'off',
-        // '@typescript-eslint/no-unsafe-assignment': 'off',
-
-        // allow debugger during development only
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    }
+    "plugins": [
+        "react"
+    ]
 }
