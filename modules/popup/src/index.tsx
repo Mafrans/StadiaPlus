5f85a3d852cb9b1f9ec0723f3a1dcc727e94a159
Mafrans <malte.kluft@gmail.com>
2021-01-24 11:22:20 +0100

    func: create base react setup for popup

---
+++ modules/popup/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import TestComponent from '../../main/src/components/TestComponent';
+import React from 'react';
+
+
+ReactDOM.render(
+    <div>
+        <h1>Stadia+</h1>
+    </div>,
+    document.getElementById('app')
+);
