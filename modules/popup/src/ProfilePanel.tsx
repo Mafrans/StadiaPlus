8fa63656a37af7375691d83e60ca5017023eb31c
Mafrans <malte.kluft@gmail.com>
2021-01-25 09:52:19 +0100

    func: flexify 💪

--- modules/popup/src/ProfilePanel.tsx
+++ modules/popup/src/ProfilePanel.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import DBProfile from '../../shared/models/DBProfile';
 import GenericButton from './GenericButton';
 import StadiaPlusDB from '../../shared/StadiaPlusDB';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 interface ProfilePanelProps {
     profile: DBProfile
 }
+
+const ProfileInfo = styled('div')`
+    ${tw`
+        flex
+    `}
+`;
 
 export default class ProfilePanel extends React.Component<ProfilePanelProps, any> {
     constructor(props: any) {
@@ -15,13 +23,17 @@ export default class ProfilePanel extends React.Component<ProfilePanelProps, any
     render() {
         return (
             <div>
-                <img
-                    src={this.props.profile.avatar}
-                    alt={`${this.props.profile.name}'s Avatar`}
-                />
+                <ProfileInfo>
+                    <img
+                        src={this.props.profile.avatar}
+                        alt={`${this.props.profile.name}'s Avatar`}
+                    />
 
-                <h3>{this.props.profile.name}</h3>
-                <p>#{this.props.profile.tag}</p>
+                    <div>
+                        <h3>{this.props.profile.name}</h3>
+                        <p>#{this.props.profile.tag}</p>
+                    </div>
+                </ProfileInfo>
 
                 <GenericButton
                     onClick={
