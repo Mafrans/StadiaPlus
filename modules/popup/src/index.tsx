6a3d1582c2aed10d822fca17aee327fba2773ce3
Mafrans <malte.kluft@gmail.com>
2021-01-26 16:38:03 +0100

    func: add Roboto as default font

--- modules/popup/src/index.tsx
+++ modules/popup/src/index.tsx
@@ -2,11 +2,12 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import '@fontsource/material-icons';
+import '@fontsource/roboto';
 import App from './App';
 
 
 ReactDOM.render(
-    <div>
+    <div style={{ fontFamily: '"Roboto", sans-serif' }}>
         <App/>
     </div>,
     document.getElementById('app')
