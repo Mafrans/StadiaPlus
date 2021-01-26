dacbf490f342e426a356412a1b470bae67270ec8
Mafrans <malte.kluft@gmail.com>
2021-01-26 17:03:12 +0100

    func: reset the default css

--- modules/popup/src/index.tsx
+++ modules/popup/src/index.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import 'reset-css';
 import 'tailwindcss/tailwind.css';
 import '@fontsource/material-icons';
 import '@fontsource/roboto';
 import App from './App';
+import styled from 'styled-components';
 
+const GlobalWrapper = styled('div')`
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+`
 
 ReactDOM.render(
-    <div style={{ fontFamily: '"Roboto", sans-serif' }}>
+    <GlobalWrapper>
         <App/>
-    </div>,
+    </GlobalWrapper>,
     document.getElementById('app')
 );
