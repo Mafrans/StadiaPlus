5418cb58fba8e4d2e6aab6d934f4fdcc04c019eb
Mafrans <malte.kluft@gmail.com>
2021-01-26 20:01:23 +0100

    func: add a support button

--- modules/popup/src/PostLoginPage.tsx
+++ modules/popup/src/PostLoginPage.tsx
@@ -4,10 +4,21 @@ import { UpdateStatus } from './components/UpdateIcon';
 import ProfilePanel from './components/ProfilePanel';
 import DBProfile from '../../shared/models/DBProfile';
 import GenericButton from './components/GenericButton';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import Theme from './Theme';
 
 interface PostLoginPageProps {
     profile: DBProfile
 }
+
+const SupportText = styled('p')`
+  ${tw`
+        text-sm
+        mt-2
+    `}
+  color: ${Theme.GRAY};
+`;
 
 export default class PostLoginPage extends React.Component<PostLoginPageProps, any> {
     constructor(props: any) {
@@ -18,11 +29,25 @@ export default class PostLoginPage extends React.Component<PostLoginPageProps, a
         return (
             <div>
                 <UpdatePanel
-                    status={ UpdateStatus.WAITING }
+                    status={UpdateStatus.WAITING}
                     title='Waiting...'
                     description='Start Stadia to begin using Stadia+'
                 />
-                <ProfilePanel profile={ this.props.profile } />
+                <ProfilePanel profile={this.props.profile} />
+                <GenericButton
+                    style={{
+                        backgroundColor: Theme.TOMATO,
+                        color: '#ffffff',
+                    }}
+                    icon='redeem'
+                >
+                    Support Stadia+
+                </GenericButton>
+                <SupportText>
+                    I'm a high school student and Stadia+ isn't free to host.
+                    If you have a dollar to spare, please consider supporting
+                    me on Patreon or Ko.fi
+                </SupportText>
             </div>
         );
     }
