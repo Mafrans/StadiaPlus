823677f299274c1bc697126afc8eef5471e94bf8
Mafrans <malte.kluft@gmail.com>
2021-02-21 11:46:03 +0100

    fix: increase performance massively by not awaiting StadiaPlusDB every time the page is loaded

--- modules/main/src/index.tsx
+++ modules/main/src/index.tsx
@@ -8,17 +8,15 @@ import GameUpdateComponent from './components/GameUpdateComponent';
 import { StadiaGameDB } from './StadiaGameDB';
 import GameTileComponent from './components/GameTileComponent';
 import Fonts from './Fonts';
-import CodecSelectComponent from './components/CodecSelectComponent';
 import GameSettingsComponent from './components/GameSettingsComponent';
 
 
 document.addEventListener('DOMContentLoaded', async () => {
     Fonts.load('Overpass', [300, 400, 700]);
 
-    await StadiaPlusDB.connect('http://localhost:3000');
-    await StadiaPlusDB.authenticate();
-    await StadiaGameDB.update();
-    await Util.updateRenderer();
+    StadiaPlusDB.connect('http://localhost:3000');
+    StadiaPlusDB.authenticate();
+    StadiaGameDB.update();
 
     const root = document.createElement('div');
     root.id = 'stadiaplus-root';
