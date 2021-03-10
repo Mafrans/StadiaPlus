3617bb60c6d4cf9f55eeedaf4fedcc8982e55aef
Mafrans <malte.kluft@gmail.com>
2021-03-10 15:27:50 +0100

    feat: upgrade the Util.desandbox method

--- modules/main/src/Util.ts
+++ modules/main/src/Util.ts
@@ -14,10 +14,58 @@ export default class Util {
         if (newRenderer != null) this.renderer = newRenderer;
     }
 
-    static desandbox(javascript: string) {
+    static desandbox(data: string | object | Function, options?: { immediate?: boolean, name?: string }): string {
         const script = document.createElement('script');
-        script.innerHTML = javascript;
+        const name = options?.name || this.randomString(8, Alphabet.LOWERCASE);
+
+        switch(typeof data) {
+            case 'string':
+                script.innerHTML = data;
+                break;
+
+            case 'object':
+                script.innerHTML = `window.${name} = ${JSON.stringify(data)};`;
+                break;
+
+            case 'function':
+                script.innerHTML = `window.${name} = ${data.toString()};`;
+                if (options?.immediate) {
+                    script.innerHTML += `window.${name}();`;
+                }
+                break;
+        }
+
+        script.innerHTML = `
+            try {
+                ${script.innerHTML}
+            }
+            catch(e) {
+                console.error(e);
+            }
+        `
+
         document.body.appendChild(script);
         script.remove();
+
+        return `window.${name}`;
     }
+
+    static randomString(length: number, alphabet?: Alphabet) {
+        let result = '';
+        const chars = alphabet || Alphabet.ASCII;
+        for (let i = 0; i < length; i++) {
+            result += chars.charAt(Math.floor(Math.random() * length));
+        }
+
+        return result;
+    }
+}
+
+export enum Alphabet {
+    NUMERIC = '1234567890',
+    LOWERCASE = 'abcdefghijklmnopqrstuvwxyz',
+    UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    LOWERCASE_NUMERIC = 'abcdefghijklmnopqrstuvwxyz1234567890',
+    UPPERCASE_NUMERIC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+    ASCII = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 }
