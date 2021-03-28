e71da18e4f6df8b8381af3c4f1f98437f39d0625
Mafrans <malte.kluft@gmail.com>
2021-03-28 12:45:40 +0200

    feat: finish styling of OnboardPanel.tsx

--- modules/popup2/src/components/OnboardPanel.tsx
+++ modules/popup2/src/components/OnboardPanel.tsx
@@ -23,12 +23,12 @@ type OnboardPanelProps = {
 export default function OnboardPanel(props: OnboardPanelProps) {
     return <Wrapper>
         <Heading>{ props.title }</Heading>
-        <p>{ props.body }</p>
+        <Paragraph>{ props.body }</Paragraph>
 
-        { props.link && <p>
+        { props.link && <Link>
             <a href={props.link.url}>{props.link.label}</a>
-            {props.link.icon}
-        </p> }
+            { props.link.icon }
+        </Link> }
 
         <Button icon={ props.button.icon } onClick={ props.button.onClick }>
             { props.button.label }
@@ -39,11 +39,39 @@ export default function OnboardPanel(props: OnboardPanelProps) {
 const Wrapper = styled.div`
     font-family: Overpass, sans-serif;
     ${tw`
+        py-4
         text-white
     `}
 `
 
 const Heading = styled.h1(tw`
+    mb-4
     text-2xl
     font-semibold
 `)
+
+const Paragraph = styled.p(tw`
+    mb-4
+    text-base
+    font-light
+`)
+
+const Link = styled.p`
+    ${tw`
+        flex
+        items-center
+        mb-4
+        text-xl
+        text-white
+    `}
+    
+    >a {
+        ${tw`
+            mt-1
+            mr-1
+            text-white
+            text-base
+            font-light
+        `}
+    }
+`
--- modules/popup2/src/index.tsx
+++ modules/popup2/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import 'reset-css';
 import '@fontsource/overpass';
+import '@fontsource/overpass/300.css';
 import '@fontsource/overpass/600.css';
 import Router from './Router';
 
