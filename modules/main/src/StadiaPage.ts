401238c6c2dc10a98bb2b09765ffdaf412e64ade
Mafrans <malte.kluft@gmail.com>
2021-04-12 22:43:58 +0200

    feat: fix fatal error in StadiaPage.ts causing it not to work properly

--- modules/main/src/StadiaPage.ts
+++ modules/main/src/StadiaPage.ts
@@ -6,21 +6,21 @@ let pathName = '';
 let page: StadiaPage = null;
 
 export const pages: { [key: string]: RegExp } = {
-    'home': /home/,
-    'player': /player\/[a-z0-9]{36}.+/
+    'home': /\/home/,
+    'player': /\/player\/[a-z0-9]{36}.+/
 }
 
 export function updatePage() {
-    console.log('updatePage');
-
     let newPathname = location.pathname;
     if(pathName === newPathname) return page;
     pathName = newPathname;
 
+    console.log('updatePage', newPathname);
+
     const keys = Object.keys(pages);
     const newPage = keys.find(key => {
         const matcher = pages[key];
-        matcher.test(pathName);
+        return matcher.test(pathName);
     }) as StadiaPage;
 
     triggerPageChangeEvent({ page: newPage, lastPage: page })
