6dc3f579a453c5b56eb7ede64219203aa207abe1
Mafrans <malte.kluft@gmail.com>
2021-04-26 17:47:42 +0200

    feat: restyle ProfilePanel.tsx

--- modules/popup2/src/components/ProfilePanel.tsx
+++ modules/popup2/src/components/ProfilePanel.tsx
@@ -4,53 +4,7 @@ import Container from './Container';
 import styled from 'styled-components';
 import { Theme } from '../../../shared/Theme';
 import { StadiaPlusDB } from '../../../shared/StadiaPlusDB';
-
-const Panel = styled.section`
-  display: flex;
-  align-items: center;
-`
-
-const ProfilePicture = styled.div`
-  position: relative;
-  width: 3rem;
-  height: 3rem;
-  color: white;
-  
-  >* {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: 999px;
-    box-sizing: border-box;
-  }
-`
-
-const Placeholder = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid ${Theme.Colors.gray['700']};
-`
-
-const ProfileInfo = styled.div`
-  font-family: Overpass, sans-serif;
-  color: white;
-  margin-left: 1rem;
-`
-
-const Name = styled.h3`
-  margin-bottom: 0.25rem;
-`
-
-const Tag = styled.p`
-  color: ${Theme.Colors.gray['500']};
-  font-size: 0.9rem;
-`
-
-const ProfileLink = styled.a`
-  color: white;
-  margin-left: auto;
-`
+import tw from 'twin.macro';
 
 interface ProfilePanelProps {
     profile: StadiaPlusDB.Profile
@@ -60,12 +14,12 @@ export default function ProfilePanel(props: ProfilePanelProps) {
     const [name, tag] = (props.profile.names[0]||'#').split('#');
 
     return <Panel>
-        <ProfilePicture>
-            { props.profile.avatar && <img src={props.profile.avatar} alt={'Profile picture'} /> }
+        <Avatar>
+            { props.profile.avatar && <AvatarImage src={props.profile.avatar} alt={'Profile picture'} /> }
             <Placeholder>
                 <CgProfile size={24} />
             </Placeholder>
-        </ProfilePicture>
+        </Avatar>
         <ProfileInfo>
             <Name>{name}</Name>
             <Tag>#{tag}</Tag>
@@ -75,3 +29,58 @@ export default function ProfilePanel(props: ProfilePanelProps) {
         </ProfileLink>
     </Panel>;
 }
+
+const Panel = styled.section(tw`
+    flex
+    py-4
+    items-center
+`)
+
+const Avatar = styled.div(tw`
+    relative
+    w-12 h-12
+    text-white`
+)
+
+const AvatarImage = styled.img(tw`
+    absolute
+    w-full h-full
+    rounded-full
+    z-10
+`)
+
+const Placeholder = styled.div`
+    ${tw`
+        flex
+        justify-center
+        items-center
+        w-full h-full
+        rounded-full
+        box-border
+    `}
+    border: 1px solid ${Theme.Colors.gray['700']};
+`
+
+const ProfileInfo = styled.div`
+    ${tw`
+        text-white
+        ml-4
+    `}
+    font-family: Overpass, sans-serif;
+`
+
+const Name = styled.h3(tw`
+    mb-1
+`)
+
+const Tag = styled.p`
+    ${tw`
+        text-sm
+    `}
+    color: ${Theme.Colors.gray['500']};
+`
+
+const ProfileLink = styled.a(tw`
+    text-white
+    ml-auto
+`)
