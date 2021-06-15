c19be9b989bf59c6438569c7fc0f7337be1b74b9
Mafrans <malte.kluft@gmail.com>
2021-06-15 02:08:58 +0200

    feat: create SettingsCategory.tsx

---
+++ modules/popup2/src/components/SettingsCategory.tsx
@@ -0,0 +1,32 @@
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { Theme } from '../../../shared/Theme';
+
+type SettingsCategoryProps = {
+    title: string
+}
+
+const SettingsCategory = (props: PropsWithChildren<SettingsCategoryProps>) => {
+    return <Wrapper>
+        <Title>{ props.title }</Title>
+        { props.children }
+    </Wrapper>;
+}
+
+const Wrapper = styled.section`
+    ${tw`
+        mt-6
+    `}
+`
+
+const Title = styled.h3`
+    ${tw`
+        text-lg
+        text-white
+        font-semibold
+    `}
+    font-family: Overpass, sans-serif;
+`
+
+export default SettingsCategory;
--- modules/popup2/src/pages/SettingsPage.tsx
+++ modules/popup2/src/pages/SettingsPage.tsx
@@ -22,6 +22,7 @@ import { Config } from '../../../shared/Config';
 import Logger from '../../../main/src/Logger';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import SettingsCategory from '../components/SettingsCategory';
 
 
 export default function SettingsPage() {
@@ -30,5 +31,9 @@ export default function SettingsPage() {
 
     return <Container>
         <Header>Settings</Header>
+
+        <SettingsCategory title={'Extension'}>
+            test
+        </SettingsCategory>
     </Container>;
 }
