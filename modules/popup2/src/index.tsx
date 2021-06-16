6adb3ff0c1f0533e9c407b270ed4b4a93d96debe
Mafrans <malte.kluft@gmail.com>
2021-06-16 23:33:24 +0200

    feat: give the popup a dark background

--- modules/popup2/src/index.tsx
+++ modules/popup2/src/index.tsx
@@ -6,8 +6,13 @@ import '@fontsource/overpass/300.css';
 import '@fontsource/overpass/600.css';
 import Router from './Router';
 import { StadiaPlusDB } from '../../shared/StadiaPlusDB';
+import { Theme } from '../../shared/Theme';
 
 StadiaPlusDB.connect('http://localhost:3000');
+
+// This is a really dumb solution, but
+// it works and shouldn't cause any issues
+document.body.style.backgroundColor = Theme.Colors.gray['900'];
 
 ReactDOM.render(
     <Router />,
